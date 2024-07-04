3d63c289c81c38c31056adfb26dee4431282bca8
moeyg <moooeyg@gmail.com>
2024-07-04 17:59:38 +0900

    feat: 앨범 페이지로 라우트 했을 때 배경색을 앨범색과 같도록 설정

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, { useEffect, useRef } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import DisplayHome from './DisplayHome';
 import DisplayAlbum from './DisplayAlbum';
+import { albumsData } from '../assets/assets';
 
 const Display: React.FC = () => {
+  const displayRef = useRef<HTMLDivElement>(null);
+  const location = useLocation();
+  const isAlbum = location.pathname.includes('album');
+  const albumID = isAlbum ? location.pathname.split('/').pop() : '';
+  const backgroundColor = albumsData[Number(albumID)].bgColor;
+
+  useEffect(() => {
+    if (isAlbum) {
+      displayRef.current.style.background = `linear-gradient(${backgroundColor}, #121212)`;
+    } else {
+      displayRef.current.style.background = `#121212`;
+    }
+  });
+
   return (
-    <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg-ml-0'>
+    <div
+      ref={displayRef}
+      className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg-ml-0'
+    >
       <Routes>
         <Route path='/' element={<DisplayHome />} />
         <Route path='/album/:id' element={<DisplayAlbum />} />
